d766852c62ecaf97530d8e147020fbeca09b1762
Ayman Ahmed Khan <aymankhan0309@gmail.com>
2024-10-14 19:12:16 +0400

    Add files via upload

---
+++ PRINTSTATEMENTS.py
@@ -0,0 +1,64 @@
+#Basic String Output
+print("Hello, World!")
+
+#Simple Math Inside Print
+a= 5
+b= 3
+print (a+b)
+
+#Multiple Arguments in Print
+print("Python", "is", "fun!")
+
+#Print with Escape Characters
+print("Ahmed said, \"Hello!\"")
+
+#Newline Character
+print ("Bath Spa\nuniversity")
+
+#Tab Character
+print("Bath\tSpa\tUniversity")
+
+#Print Multiple Data Types Together
+print("The result is", 42)
+
+#Print a Single Number
+print(12345)
+
+#Print a Decimal Number
+print(3.14159)
+
+#Print Special Characters
+print("Dollar sign: $")
+
+#Using the sep Parameter
+print("apples", "banana", "cherry", sep=", ")
+
+#Using the end Parameter
+print("Hello,", end=" ")
+print("World!")
+
+#Print a Repeated String
+print("Python!" * 3)
+
+#Print a Unicode Character
+print("\u2764") # Prints a heart symbol
+
+#Print Empty Line
+print()
+
+#Concatenating Strings Inside Print
+print("Hello " + "World!")
+
+#Print Boolean Values
+print(True)
+print(False)
+
+#Printing Exponential Notation
+print(1.2e3) # Prints 1200.0
+
+#Print Multiline Text Using Triple Quotes
+print("""Bath spa
+University.""")
+
+#Print Using Escaped Backslash
+print("This is BSU University: \\")
